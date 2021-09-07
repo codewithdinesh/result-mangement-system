9d2e72206386ea8c9b4b8853a9be685b0d4151cf
Dinesh <pr760677@gmail.com>
2021-09-07 21:32:31 +0530

    marsheet json file added to

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var app = express()
 var fs = require('fs')
 var http = require('http').Server(app);
 
-// var http=require('http')
-// server = require('http').createServer(app)
-// // io = io.listen(server);
 
 http.listen(process.env.PORT || 3000, function () {
     console.log('listening on', http.address().port);
@@ -25,39 +22,42 @@ app.get('/', function (req, response) {
     </head>
     <body>`);
     response.write(` <div class="container">
-    <div class="header">
-
-    </div>
-    <div class="input_box my-2 mx-2">
-        <div class="mb-3 row">
-            
-            <div class="col-md-10 my-1">
-              <input type="text" class="form-control" id="search_term">
+        <div class="header">
+        <h1 class="text-center" style="color: rgb(255, 81, 0);">Check Exam Result</h1>
+        </div>
+        <div class="input_box my-2 mx-2">
+            <div class="mb-3 row">
+                
+                <div class="col-md-10 my-1">
+                <input type="text" class="form-control" id="search_term" placeholder="Enter Enrollment No.">
+                </div>
+                <button  class="col-md-2 col-form-label btn btn-primary my-1">Search</button>
             </div>
-            <button  class="col-md-2 col-form-label btn btn-primary my-1">Search</button>
-          </div>
-    </div>
-</div>`);
+        </div>
+    </div>`);
+
     response.write(` <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-</body>
-</html>`);
+    </body>
+    </html>`);
 
 });
 
-app.get('/users', function (req, res) {
-    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+
+app.get('/marksheet', function (req, res) {
+    fs.readFile(__dirname + "/" + "marksheet.json", 'utf8', function (err, data) {
         console.log(data);
         res.end(data);
     });
 })
 
-app.get('/users/:id', function (req, res) {
+app.get('/marksheet/:id', function (req, res) {
 
     // First read existing users.
-    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+    fs.readFile(__dirname + "/" + "marksheet.json", 'utf8', function (err, data) {
         var users = JSON.parse(data);
-        var user = users["user" + req.params.id]
+        var user = users["students" + req.params.id]
         console.log(user);
+        
         res.end(JSON.stringify(user));
 
     });
