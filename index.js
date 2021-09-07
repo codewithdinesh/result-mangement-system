592372b16368846a69c771178cdc3f0f7a13a342
Dinesh <pr760677@gmail.com>
2021-09-07 23:59:31 +0530

    hiding all btn outline

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.get('/', function (req, response) {
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <title>Check Exam Result </title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <style>
+        .btn:focus {
+            outline: none;
+          }
+        </style>
     </head>
     <body>`);
     response.write(` <div class="container">
