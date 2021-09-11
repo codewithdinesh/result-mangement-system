f90c9d29ae274c067861c936f1db46e5807a389a
Dinesh <pr760677@gmail.com>
2021-09-11 11:43:44 +0530

    Search bar designed and modifieded

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ app.get('/', function (req, response) {
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <title>Check Exam Result </title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <style>
+        .btn{
+            background-color:#294a70;
+            color:white;
+        }
+        .btn:hover{
+            background-color:#11498a;
+            color:white;
+
+        }
+        </style>
        
     </head>
     <body>`);
@@ -45,7 +56,11 @@ app.get('/', function (req, response) {
                     <div class="col-md-10 my-1">
                         <input type="text" name="search_id" class="form-control shadow-none" id="search_term" placeholder="Enter Enrollment No." required>
                     </div>
-                        <button  class="col-md-2 name="submit_btn" type="submit" col-form-label btn btn-primary my-1 shadow-none">Search</button>
+                            <button  class="btn btn-md btn-default col-md-2  col-form-label  my-1 shadow-none" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-align-center" viewBox="0 0 16 16">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                    </svg>
+                            Search</button>
                 </div>
             </form>
         </div>
@@ -135,8 +150,8 @@ app.get('/marksheet', function (req, res) {
                     <div class="col-12">
                     
                         <a href="/">
-                            <button class="btn btn-primary">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                            <button class="btn btn-success text-center ">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                                     <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                                 </svg>
                                 Go Back to Search Page
