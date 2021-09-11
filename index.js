e2c45528dc5ef8c6b7da6344dada002a3d33e566
Dinesh <pr760677@gmail.com>
2021-09-11 20:19:48 +0530

    Marksheet Designed Finaly,, 12 user data now awalable on this website

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ var data = require('./data/marksheet.json');
 app.get('/', function (req, response) {
 
     response.writeHead(200, { 'Content-Type': 'text/html' });
+ 
 
     //Head of Html document
     response.write(`<!DOCTYPE html>
@@ -50,12 +51,15 @@ app.get('/', function (req, response) {
     <body>`);
 
     response.write(` <div class="container">
-        <div class="header">
-        <h1 class="text-center" style="color: rgb(255, 81, 0);">Check Exam Result</h1>
+        
+    <div class="align-items-center h-100">
+        <div class="header col-md-12">
+        
+        <h1 class="text-center" style="color: rgb(255, 81, 0);">Check Exam Result </h1>
         </div>
-        <div class="input_box my-2 mx-2">
+        <div class="input_box my-2 mx-2 col-md-12">
              <form action="/marksheet" method="GET">
-                <div class="mb-3 row">
+                <div class="mb-3 row align-items-center h-100">
                 
                     <div class="col-md-10 my-1">
                         <input type="text" maxlength="10" name="search_id" class="form-control shadow-none" id="search_term" placeholder="Enter Enrollment No." required>
@@ -67,6 +71,7 @@ app.get('/', function (req, response) {
                             Search</button>
                 </div>
             </form>
+        </div>
         </div>
     </div>`);
 
@@ -112,32 +117,37 @@ app.get('/marksheet', function (req, res) {
         }
 
     }
+    console.log(studentsArray)
 
     //find out students marksheet by its id so i used my own function to find out user by its id
     var student = findId(studentsArray, search_term);
 
     //setting html 
-    res.write(`
-         <!DOCTYPE html>
-         <html lang="en">
-         <head>
-             <meta charset="UTF-8">
-             <meta http-equiv="X-UA-Compatible" content="IE=edge">
-             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-             <link rel="shortcut icon" href="/favicon" type="image/x-icon">
-             <title>Marksheet</title>
-             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-             <style>
-             .btn:focus,.btn:active ,input:focus ,input:active {
-                 outline: none !important;
-                 box-shadow: none;
-              }
-             </style>
-         </head>
-         <body>
-         `);
+
+
+
 
     if (student === undefined) {
+
+        res.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="shortcut icon" href="/favicon" type="image/x-icon">
+            <title>Marksheet</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+            <style>
+            .btn:focus,.btn:active ,input:focus ,input:active {
+                outline: none !important;
+                box-shadow: none;
+             }
+            </style>
+        </head>
+        <body>
+        `);
         if (search_term != undefined) {
             if (search_term.length < 10) {
                 res.write(`
@@ -196,6 +206,106 @@ app.get('/marksheet', function (req, res) {
     }
     else {
 
+        //html head 
+        res.write(`
+        <!DOCTYPE html>
+        <html>
+        
+        <head>
+            <title> </title>
+        
+            <style>
+                body {
+                    font-family: Calibri;
+                    margin: 10px;
+                }
+        
+                .statment {
+                    font-family: Calibri;
+                    text-align: center;
+                    margin-top: 20px;
+                    text-transform: uppercase;
+                }
+                .column-1{
+                    height:30px;
+                    padding-left:15px
+                }
+        
+        
+                .tbl tr td {
+                    padding: 2px 5px;
+                }
+        
+        
+                * {
+                    margin: 0px;
+                    padding: 0px;
+        
+                }
+        
+                .main {
+                    width: 1050px;
+                    height: auto;
+                    border: 3px dotted black;
+                    margin: auto;
+                    padding: 10px;
+                }
+        
+        
+                .heading {
+                    width: 100%;
+                    height: 100px;
+                }
+        
+        
+                .heading h1,
+                h3 {
+        
+                    font-family: 'Old English Text MT';
+                    text-align: center
+                }
+        
+        
+                .img {
+                    width: 10%;
+                    height: 100px;
+                    float: left;
+                    margin-left: 15px;
+                }
+        
+                .td {
+        
+                    height: 15px;
+                    width: 205px;
+                    text-align: center;
+                }
+        
+                .td1 {
+                    /*border: 1px solid;*/
+                    height: 15px;
+                    width: 80px;
+                    text-align: center;
+        
+        
+                }
+        
+                table,
+                th,
+                td {
+                    border: 1px solid black;
+                    border-collapse: collapse;
+                }
+        
+                .col {
+                    border: 0px;
+                    border-bottom: 1px dashed;
+                    border-collapse: collapse;
+                    height: 40px;
+                }
+            </style>
+        
+        </head>
+             `);
 
 
 
@@ -203,39 +313,102 @@ app.get('/marksheet', function (req, res) {
 
         //Setup Marksheet Page
 
+        res.write(`<body>
 
+        <div class="main">
+            <img class="img" src="https://msbte.org.in/MSBTE_Logo/logo.png" border="0" alt="MSBTE LOGO" />
+    
+            <div class="heading">
+                <h1 style="font-size:41px;">Maharashtra State Board of Technical Education</h1>
+                <h3 class="statment">Statement of Marks</h3>
+            </div>
+    
+            <div>`)
 
         res.write(` 
-        <div class="marksheet container">
+        <table style="width:100%;margin-top:10px;border:0px solid">
+                <tr>
+                    <td style="width:155px;" class="col">&nbsp;&nbsp;&nbsp;<strong>MR. / MS.</strong></td>
+                    <td colspan="6" class="col"><strong>${student.name}</strong></td>
+                </tr>
 
-        <div class="head">
+                <tr>
+                    <td style="width:155px;" class="col">&nbsp;&nbsp;&nbsp;<strong>ENROLLMENT NO.</strong></td>
+                    <td style="width:170px;" class="col">${student.id}</td>
+                    <td style="width:150px;" class="col"><strong>EXAMINATION</strong></td>
+                    <td style="width:150px;" class="col">${student.exam}</td>
+                    <td style="width:100px;" class="col"><strong>SEAT NO.</strong></td>
+                    <td style="width:100px;" class="col">${student.seat}</td>
+                    <td style="width:100px;" class="col"><strong> ${student.sem}</strong></td>
+                </tr>
 
-            <h4 class="text-center text-uppercase top-header-statement">Statemant of marks</h4>
+                <tr>
+                    <td style="margin-right:20px;width:155px;" class="col">&nbsp;&nbsp;&nbsp;<strong>COURSE</strong>
+                    </td>
+                    <td colspan="6" class="col">${student.course}</td>
+                </tr>
+        </table>
+    `);
 
-        </div>
+        //console
 
-        <hr/>
 
-        <div class="details">`);
+        res.write(`  <div id="dvMain0">
+            <table border='1' cellspacing='0' align='center' widht='0'>
+                <tr>
+                    <td width='700px' height='30px' align='center' rowspan='2'><strong>TITLE OF COURSES</strong>
+                    </td>
+
+                    </td>
+                    <td width='350px' height='30px' align='center' colspan='5'><strong>MARKS</strong></td>
+                    <!-- <td width='150px' height='30px align='center' colspan='2'><strong></strong></td> -->
+
+                </tr>
+                <tr>
+                    <td width='150px' height='30px' align='center'><strong>MAX.</strong></td>
+
+                    <td width='150px' height='30px' align='center'><strong>OBT</strong></td>
+                </tr>
+                `);
+
+
+        //set total marks =0
+        var totalmarks = 0;
+
+        //length of subjects
+        var nosub = student.subject.length;
+
+        //users total obtained marks
+        var usermarks = 0;
+
+        //Printing marks and Subject ,calculating totalmarks
+        for (var i = 0; i < student.subject.length; i++) {
+
+            //calculating max. total marks
+            totalmarks += student.subject[i].maxmarks;
+
+            //calculating user obtained total marks
+            usermarks += student.subject[i].marks;
+
+            res.write(`<tr>
+                    <td  class="column-1">
+                    ${student.subject[i].name}
+                    </td>
+                    <td class="column-1">
+                    ${student.subject[i].maxmarks}
+                    </td>
+                    <td class="column-1">
+                    ${student.subject[i].marks}
+                    </td>
+                      
+                    </tr>`)
+
+        }
 
         res.write(`
-        <div class="name">
-        ${student.name}
-        </div>
-        <div class="enroll">
-        ${student.id}
-        </div>
-        <div class="seat">
-        ${student.seat}
-        </div>
 
-        <div class="sem">
-        ${student.sem}
-        </div>
-        <div class="course">
-        ${student.marks}
-        </div>
-    `);
+            </table>
+        </div>`)
 
 
 
@@ -244,11 +417,64 @@ app.get('/marksheet', function (req, res) {
 
         //Ending Html in marksheet search page
         res.write(`
-        </div>
-        </div>
-    </body>
-    </html>
-`)
+        <div id="dvTotal0">
+                <table class='tbl' border='1' cellspacing='0' align='center' widht='0'>
+                    <tr>
+                        <td width='150px' height='60px' align='left' rowspan='3'><strong>DATE :</strong><br />
+                            ${student.date} </td>
+                        <td width='600px' height='30px' align='center' rowspan='3'>This Marksheet is Downloaded from
+                            Internet<br /></br><span style='font-weight:bold;'> SECRETARY </span><br />MAHARASHTRA STATE
+                            BOARD OF TECHNICAL EDUCATION</td>
+                        <td width='150px' height='30px' align='center'><strong>TOTAL MAX.<br /> MARKS</strong></td>
+                        <td width='150px' height='30px' align='center'><strong>RESULT WITH<br />%</strong></td>
+                        <td width='150px' height='30px' align='center'><strong>TOTAL MARKS<br /> OBTAINED </strong></td>
+
+                    </tr>`
+        );
+
+
+        var percentage = usermarks / totalmarks * 100;
+        var percentage1 = percentage.toFixed(2);
+
+        var dist;
+        if (percentage1 >= 75) {
+            dist = "First class Dist."
+        } else if (percentage1 >= 60 && percentage1 < 75) {
+            dist = "Second class"
+        } else if (percentage1 >= 50 && percentage1 < 60) {
+            dist = "Third Class"
+        }
+        else if (percentage1 >= 35 && percentage1 < 50) {
+            dist = "pass"
+        }
+        else {
+            dist = "fail"
+        }
+
+        res.write(`
+                    <tr>
+                    <td width='150px' height='30px' align='center'><strong>${totalmarks}</strong></td>
+                    <td width='150px' height='30px' align='center'><strong>${percentage1}</strong></td>
+                    <td width='150px' height='30px' align='center'><strong>${usermarks}</strong></td>
+                    
+                    <tr>
+                        
+                        <td width='150px' height='30px' align='center' colspan='4'><strong>${dist}</strong>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div>
+`);
+        res.write(` <p style="page-break-after: always;">&nbsp;</p>
+        <!--<p style="page-break-before: always;">&nbsp;</p>-->
+
+        <br />
+        <br>
+</body>
+
+</html>`);
 
         //Ending else part of Marksheet id is blank or not
     }
@@ -288,7 +514,7 @@ app.get('/marksheet/:id', function (req, res) {
 
         var user = findId(marksheet, uid)
 
-        
+
 
 
         res.write(`
@@ -355,22 +581,22 @@ app.get('/marksheet/:id', function (req, res) {
                         </div>
                     </div>               
                     </div>`)
-    
-    
-    
+
+
+
                 }
-    
-    
-    
-    
+
+
+
+
             }
-    
-    
-    
+
+
+
         }
         else {
 
-        res.write(` 
+            res.write(` 
             <div class="marksheet container">
 
             <div class="head">
@@ -383,8 +609,8 @@ app.get('/marksheet/:id', function (req, res) {
 
             <div class="details">`);
 
-        var name = user.name;
-        res.write(`
+            var name = user.name;
+            res.write(`
             <div class="name">
             ${name}
             </div>
@@ -427,7 +653,7 @@ function findId(data, fid) {
 
     var i = 0;
     // console.log("length:" + studentsArray.length)
-    while (i < studentsArray.length - 1) {
+    while (i < studentsArray.length) {
 
         // console.log("loop checking: " + i)
 
